3c15aad5c0cad814caec56caec794b91c5a73b32
Denis Turbiez <denisft77@gmail.com>
2023-06-24 14:39:26 +0200

    Fix import

--- m-infrastructure/src/main/kotlin/com/esgi/infrastructure/gateways/MessagesGateway.kt
+++ m-infrastructure/src/main/kotlin/com/esgi/infrastructure/gateways/MessagesGateway.kt
@@ -1,18 +1,12 @@
 package com.esgi.infrastructure.gateways
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.messaging.MessageHeaders
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
-import org.springframework.messaging.simp.annotation.SubscribeMapping
-import org.springframework.messaging.support.MessageBuilder
-import org.springframework.security.access.annotation.Secured
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.CrossOrigin
-import java.security.Principal
 import java.util.UUID
 
 @Controller
