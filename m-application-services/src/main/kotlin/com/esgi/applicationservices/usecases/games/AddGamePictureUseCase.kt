86c6c8d91cbaa2935b40cc0f8aa1cd37663a9659
Loïc Vanden Bossche <loloic066@gmail.com>
2023-07-20 23:31:54 +0200

    Added rights to games actions

--- m-application-services/src/main/kotlin/com/esgi/applicationservices/usecases/games/AddGamePictureUseCase.kt
+++ m-application-services/src/main/kotlin/com/esgi/applicationservices/usecases/games/AddGamePictureUseCase.kt
@@ -4,7 +4,10 @@ import com.esgi.applicationservices.persistence.GamesPersistence
 import com.esgi.applicationservices.services.GamePictureUploader
 import com.esgi.domainmodels.Game
 import com.esgi.domainmodels.exceptions.BadRequestException
+import com.esgi.domainmodels.exceptions.ForbiddenException
+import com.esgi.domainmodels.exceptions.NotFoundException
 import java.io.InputStream
+import java.util.UUID
 
 class AddGamePictureUseCase(
     private val gamePictureUploader: GamePictureUploader,
@@ -16,7 +19,13 @@ class AddGamePictureUseCase(
         "image/gif"
     )
 
-    operator fun invoke(gameId: String, data: InputStream, contentType: String?): Game {
+    operator fun invoke(ownerId: UUID, gameId: String, data: InputStream, contentType: String?): Game {
+        val game = gamesPersistence.findById(gameId) ?: throw NotFoundException("Game not found")
+
+        if (game.owner.id == ownerId) {
+            throw ForbiddenException("You are not the owner of this game")
+        }
+
         if (contentType !in contentTypes) {
             throw BadRequestException("Content type $contentType is not allowed")
         }
